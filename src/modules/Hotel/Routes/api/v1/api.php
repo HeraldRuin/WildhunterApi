8b7e5388d713bc4f72b3367e8e57d720fb878306
melvin <melvin.melvin.rulit@gmail.com>
2026-07-03 09:12:04 +0300

    маршрут поиска отелей

--- src/modules/Hotel/Routes/api/v1/api.php
+++ src/modules/Hotel/Routes/api/v1/api.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Modules\Hotel\Controllers\HotelController;
 
 Route::post('/hotels/offers', [HotelController::class, 'getHotels']);
+Route::post('/hotels/search', [HotelController::class, 'searchHotels']);
 
 Route::middleware('auth:sanctum')->group(function () {
 
