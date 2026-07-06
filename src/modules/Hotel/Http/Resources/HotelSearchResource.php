f88d5c06652494d310bfd88b3e00e2e2ea1a8e53
melvin <melvin.melvin.rulit@gmail.com>
2026-07-06 22:58:16 +0300

    поля в ресурсе поиска отелей

--- src/modules/Hotel/Http/Resources/HotelSearchResource.php
+++ src/modules/Hotel/Http/Resources/HotelSearchResource.php
@@ -14,9 +14,10 @@ class HotelSearchResource extends BaseJsonResource
             'title'=> $this->resource->title,
             'slug'=> $this->resource->slug,
             'image_url' => $this->resource->getImageUrl()?: asset('uploads/0000/1/2026/11/14/no_image.png'),
-            'review_score' => $this->resource->review_score,
+            'price' => $this->resource->price,
+            'review_count' => $this->resource->reviews->count(),
             'star_rate' => $this->resource->star_rate,
-            'has_wish_list' => $this->hasWishList !== null,
+            'is_in_wishList' => $this->hasWishList !== null,
             'location' => LocationResource::make($this->resource->location),
         ];
     }
