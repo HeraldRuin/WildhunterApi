7cfc9eed2fba6f2df0440cfc35d5731e91acf74d
melvin <melvin.melvin.rulit@gmail.com>
2026-07-06 15:06:44 +0300

    сервис поиска отелей - тут главная точка поиска

---
+++ src/modules/Hotel/Services/HotelSearchService.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Modules\Hotel\Services;
+
+use Illuminate\Pipeline\Pipeline;
+use Modules\Hotel\Dto\HotelSearchData;
+use Modules\Hotel\Models\Hotel;
+
+class HotelSearchService
+{
+    public function search(HotelSearchData $dto)
+    {
+        $userId = auth()->id();
+
+        $query = Hotel::query()
+            ->select('bc_hotels.*')
+            ->where('bc_hotels.status', 'publish');
+
+        $payload = app(Pipeline::class)
+            ->send([
+                'query' => $query,
+                'dto' => $dto,
+            ])
+            ->through($this->filters())
+            ->thenReturn();
+
+        return $payload['query']
+            ->groupBy('bc_hotels.id')
+            ->with([
+                'location',
+                'hasWishList' => function ($q) use ($userId) {
+                    $q->where('user_id', $userId);
+                }
+//                'translation',
+            ]);
+    }
+
+    private function filters(): array
+    {
+        return [
+            \Modules\Hotel\Filters\LocationFilter::class,
+//            \App\Services\Hotel\Filters\PriceFilter::class,
+//            \App\Services\Hotel\Filters\AnimalFilter::class,
+//            \App\Services\Hotel\Filters\StarRateFilter::class,
+            \Modules\Hotel\Filters\DateAvailabilityFilter::class,
+        ];
+    }
+}
