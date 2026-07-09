1d10e6fd6c04818b70bd8ea9eacab85676c09b55
melvin <melvin.melvin.rulit@gmail.com>
2026-07-09 15:08:50 +0300

    получение ролей без аутентификации

--- src/modules/Role/Routes/api/v1/api.php
+++ src/modules/Role/Routes/api/v1/api.php
@@ -3,8 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Role\Controllers\RoleController;
 
+Route::get('/roles', [RoleController::class, 'roles']);
+
 Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/roles', [RoleController::class, 'roles']);
     Route::get('/roles/{id}', [RoleController::class, 'getById'])->whereNumber('id');
     Route::get('/roles/code/{code}', [RoleController::class, 'getByCode']);
     Route::get('/roles/user/{user}', [RoleController::class, 'getUserRole']);
