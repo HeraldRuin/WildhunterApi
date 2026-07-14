9405085d9615bd21f632656e9c88c208c9e55c4c
melvin <melvin.melvin.rulit@gmail.com>
2026-07-14 14:34:38 +0300

    файл валидации  для атрибутов

---
+++ src/modules/Attributes/Http/Request/ServiceAttributesRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Modules\Attributes\Http\Request;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ServiceAttributesRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'type' => ['required', 'string', 'max:255'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'type.required' => __('review.validation.type_required'),
+            'type.string' => __('review.validation.type_string'),
+        ];
+    }
+}
