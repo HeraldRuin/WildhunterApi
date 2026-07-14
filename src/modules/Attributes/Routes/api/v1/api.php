4d5c0118a659804ef55b42779d0d5ef81dfb7e79
melvin <melvin.melvin.rulit@gmail.com>
2026-07-14 09:14:26 +0300

    маршрут получения атрибутов

---
+++ src/modules/Attributes/Routes/api/v1/api.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Modules\Attributes\Controllers\AttributesController;
+
+Route::get('/hotels/attributes', [AttributesController::class, 'getHotelAttributes']);
+
+Route::middleware('auth:sanctum')->group(function () {
+
+});
