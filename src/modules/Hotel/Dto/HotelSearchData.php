2888947980fc4d0b03cd79e69f62d4fffe8e717e
melvin <melvin.melvin.rulit@gmail.com>
2026-07-15 12:02:52 +0300

    добавлен в dto свойства атрибутов

--- src/modules/Hotel/Dto/HotelSearchData.php
+++ src/modules/Hotel/Dto/HotelSearchData.php
@@ -8,7 +8,6 @@ class HotelSearchData
 {
     public function __construct(
         public ?int $location_id,
-        public ?array $locationIds,
         public ?int $animal_id,
         public string $startDate,
         public string $endDate,
@@ -16,6 +15,7 @@ class HotelSearchData
         public int $children,
         public ?array $star_rate,
         public ?array $price,
+        public ?array $termIds,
         public ?string $order_by,
         public ?string $order_direction,
         public ?int $limit,
@@ -27,7 +27,6 @@ class HotelSearchData
 
         return new self(
             location_id: $data['location_id'] ?? null,
-            locationIds: $data['location_ids'] ?? null,
             animal_id: $data['animal_id'] ?? null,
             startDate: $data['check_in'],
             endDate: $data['check_out'],
@@ -35,6 +34,7 @@ class HotelSearchData
             children: $data['children'] ?? 0,
             star_rate: $data['star_rate'] ?? null,
             price: $data['price'] ?? null,
+            termIds: $data['term_ids'] ?? null,
             order_by: $data['order_by'] ?? null,
             order_direction: $data['order_direction'] ?? null,
             limit: $data['limit'] ?? null,
