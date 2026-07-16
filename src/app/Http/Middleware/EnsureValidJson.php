30f7647b103d488eb4798d0a266ec183b9f4afaf
melvin <melvin.melvin.rulit@gmail.com>
2026-07-16 12:34:31 +0300

    поправка выбора перевода

--- src/app/Http/Middleware/EnsureValidJson.php
+++ src/app/Http/Middleware/EnsureValidJson.php
@@ -28,7 +28,7 @@ class EnsureValidJson
                 'message' => 'Ошибка валидации',
                 'error_code' => 'validation_error',
                 'errors' => [
-                    'body' => [__('core.invalid_json_body')],
+                    'body' => [__('core.errors.invalid_json_body')],
                 ],
                 'trace_id' => $request->attributes->get('trace_id'),
             ], 422);
