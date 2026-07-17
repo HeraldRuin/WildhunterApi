d2ec063c8a0343d8d14aa0f05998d1ea52b7b92e
melvin <melvin.melvin.rulit@gmail.com>
2026-07-17 12:33:17 +0300

    маршрут создания брони

--- src/modules/Booking/Routes/api/v1/api.php
+++ src/modules/Booking/Routes/api/v1/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Booking\Controllers\BookingController;
 
+Route::post('/bookings', [BookingController::class, 'store']);
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/users/{id}/bookings', [BookingController::class, 'bookingHistory']);
- });
+});
