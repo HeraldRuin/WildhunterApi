7c5c120d75a921342156c2246420317d64666744
melvin <melvin.melvin.rulit@gmail.com>
2026-07-17 13:02:16 +0300

    маршрут создания брони под защитой sanctum

--- src/modules/Booking/Routes/api/v1/api.php
+++ src/modules/Booking/Routes/api/v1/api.php
@@ -3,8 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Booking\Controllers\BookingController;
 
-Route::post('/bookings', [BookingController::class, 'store']);
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/bookings', [BookingController::class, 'store']);
     Route::get('/users/{id}/bookings', [BookingController::class, 'bookingHistory']);
 });
