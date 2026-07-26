7371abc07a69b53325b4b652d44cda858a436867
melvin <melvin.melvin.rulit@gmail.com>
2026-07-26 17:47:56 +0300

    роут получения отелей локации

--- src/modules/Location/Routes/api/v1/api.php
+++ src/modules/Location/Routes/api/v1/api.php
@@ -5,6 +5,7 @@ use Modules\Location\Controllers\LocationController;
 
 Route::post('/locations/offers', [LocationController::class, 'getBestLocations']);
 Route::get('/locations', [LocationController::class, 'getLocations']);
+Route::get('/locations/{id}/hotels', [LocationController::class, 'getLocationHotels']);
 
 Route::middleware('auth:sanctum')->group(function () {
 
