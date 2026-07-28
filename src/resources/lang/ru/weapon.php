3a18740c4a4367851238d81782dbf06761b5a706
melvin <melvin.melvin.rulit@gmail.com>
2026-07-28 22:13:16 +0300

    перевод

--- src/resources/lang/ru/weapon.php
+++ src/resources/lang/ru/weapon.php
@@ -10,6 +10,10 @@ return [
 
     ],
     'validation' => [
+        'hunter_billet_number_required' => 'Номер охотничьего билета обязателен, если не переданы данные оружия',
+        'hunter_billet_number_string' => 'Номер охотничьего билета должен быть строкой',
+        'hunter_billet_number_max' => 'Номер охотничьего билета не должен превышать 255 символов',
+
         'hunter_license_number_required' => 'Номер лицензии обязателен',
         'hunter_license_number_string' => 'Номер лицензии должен быть строкой',
 
