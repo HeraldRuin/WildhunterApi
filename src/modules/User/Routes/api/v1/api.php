f9e2278013742eb02e564a57c69d5326e196199f
melvin <melvin.melvin.rulit@gmail.com>
2026-07-28 20:33:10 +0300

    маршрута и перенес

--- src/modules/User/Routes/api/v1/api.php
+++ src/modules/User/Routes/api/v1/api.php
@@ -5,6 +5,7 @@ use Modules\User\Controllers\AuthController;
 use Modules\User\Controllers\PasswordController;
 use Modules\User\Controllers\UserController;
 use Modules\User\Controllers\UserWishListController;
+use Modules\Weapon\Controllers\WeaponController;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
@@ -25,7 +26,13 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/users', [UserController::class, 'searchUsers']);
     Route::get('/user/current-password', [PasswordController::class, 'getCurrentPassword']);
     Route::post('/user/change-password', [PasswordController::class, 'updatePassword']);
-    Route::get('/user/{user}', [UserController::class, 'searchUser']);
+
+    Route::get('/user/weapons', [WeaponController::class, 'index']);
+    Route::post('/user/weapons', [WeaponController::class, 'store']);
+    Route::put('/user/weapons/{id}', [WeaponController::class, 'update']);
+    Route::delete('/user/weapons/{id}', [WeaponController::class, 'destroy']);
+
+    Route::get('/user/{user}', [UserController::class, 'searchUser'])->whereNumber('user');
     Route::post('/user', [UserController::class, 'profileUpdate']);
 
     //Избранное
