ff32eaeb94702b1a8f9f2796e9195046a0d930ec
melvin <melvin.melvin.rulit@gmail.com>
2026-07-31 16:12:23 +0300

    номер билета убираю из ресурса оружий

--- src/modules/Weapon/Http/Resources/UserWeaponResource.php
+++ src/modules/Weapon/Http/Resources/UserWeaponResource.php
@@ -10,7 +10,6 @@ class UserWeaponResource extends BaseJsonResource
     {
         return [
             'id' => $this->resource->id,
-            'hunter_billet_number' => $this->resource->user?->hunter_billet_number,
             'hunter_license_number' => $this->resource->hunter_license_number,
             'hunter_license_date' => $this->resource->hunter_license_date?->translatedFormat('d F Y г.'),
             'weapon_type' => $this->resource->type?->title,
