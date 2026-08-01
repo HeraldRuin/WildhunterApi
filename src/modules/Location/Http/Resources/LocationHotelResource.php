87bc8fceb13b81b1f5de1c6abcae643b99fd3cfc
melvin <melvin.melvin.rulit@gmail.com>
2026-08-01 21:49:13 +0300

    координаты отелей

--- src/modules/Location/Http/Resources/LocationHotelResource.php
+++ src/modules/Location/Http/Resources/LocationHotelResource.php
@@ -16,6 +16,8 @@ class LocationHotelResource extends BaseJsonResource
             'id' => $hotel->id,
             'title' => $hotel->title,
             'slug' => $hotel->slug,
+            'map_lat'=> $this->resource->map_lat,
+            'map_lng'=> $this->resource->map_lng,
             'image_url' => $hotel->getImageUrl('medium') ?: asset('uploads/0000/1/2026/11/14/no_image.png'),
             'star_rate' => $hotel->star_rate,
             'price' => $hotel->price,
