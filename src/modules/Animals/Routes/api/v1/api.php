fc2f522356d0d793261b711c0d78b529400661d9
melvin <melvin.melvin.rulit@gmail.com>
2026-08-05 20:32:02 +0300

    маршрут получение доступности животных

--- src/modules/Animals/Routes/api/v1/api.php
+++ src/modules/Animals/Routes/api/v1/api.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Modules\Animals\Controllers\AnimalController;
 
 Route::get('/animals', [AnimalController::class, 'getAnimals']);
+Route::post('/animals/check-availability', [AnimalController::class, 'checkAvailability']);
 
 Route::middleware('auth:sanctum')->group(function () {
 
