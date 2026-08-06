8c9be30c418f5494f720c263297f5b54cc5cdd0c
melvin <melvin.melvin.rulit@gmail.com>
2026-08-06 15:45:20 +0300

    поправка валидации особых требований

--- src/modules/Booking/Http/Requests/UpdateCustomerNotesRequest.php
+++ src/modules/Booking/Http/Requests/UpdateCustomerNotesRequest.php
@@ -9,13 +9,18 @@ class UpdateCustomerNotesRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'customer_notes' => ['nullable', 'string', 'max:5000'],
+            'code' => ['required', 'string'],
+            'customer_notes' => ['required', 'string', 'max:5000'],
         ];
     }
 
     public function messages(): array
     {
         return [
+            'code.required' => __('booking.validation.code_required'),
+            'code.string' => __('booking.validation.code_must_be_string'),
+
+            'customer_notes.required' => __('booking.validation.customer_notes_required'),
             'customer_notes.string' => __('booking.validation.customer_notes_must_be_string'),
             'customer_notes.max' => __('booking.validation.customer_notes_max'),
         ];
