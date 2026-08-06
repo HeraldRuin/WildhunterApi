a22a0a674c01945fee9df78c165c29a3d9add677
melvin <melvin.melvin.rulit@gmail.com>
2026-08-06 17:40:57 +0300

    поправка dto создания брони

--- src/modules/Booking/Dto/CreateBookingData.php
+++ src/modules/Booking/Dto/CreateBookingData.php
@@ -35,7 +35,7 @@ class CreateBookingData
                     roomId: (int) $room['room_id'],
                     number: (int) $room['number'],
                 ),
-                $data['rooms']
+                $data['rooms'] ?? []
             ),
         );
     }
