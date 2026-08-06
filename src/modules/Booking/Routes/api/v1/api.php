e41b4b2bebff63b06a0a502cb8943234b3b4a51d
melvin <melvin.melvin.rulit@gmail.com>
2026-08-06 11:20:39 +0300

    маршрут получения истории

--- src/modules/Booking/Routes/api/v1/api.php
+++ src/modules/Booking/Routes/api/v1/api.php
@@ -3,12 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Booking\Controllers\BookingController;
 
-
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/bookings', [BookingController::class, 'store']);
 
+    Route::get('/bookings/history', [BookingController::class, 'bookingHistory']);
+
     Route::get('/bookings/{code}/checkout', [BookingController::class, 'checkout']);
     Route::post('/bookings/{code}/checkout', [BookingController::class, 'doCheckout']);
-
-    Route::get('/users/{id}/bookings', [BookingController::class, 'bookingHistory']);
 });
