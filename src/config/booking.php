4ecac243860d461935dad64c31a468883d938de8
melvin <melvin.melvin.rulit@gmail.com>
2026-08-07 21:15:49 +0300

    статусы в конфигурации бронирований

---
+++ src/config/booking.php
@@ -0,0 +1,17 @@
+<?php
+return [
+    'statuses'=>[
+        'completed',
+        'processing',
+        'confirmed',
+        'cancelled',
+        'paid',
+        'unpaid',
+        'partial_payment',
+        'collection',
+        'invitation',
+        'prepayment_collection',
+        'bed_collection',
+        'finish_bed_collection',
+    ]
+];
