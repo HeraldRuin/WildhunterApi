de7551107b09e559c032a14051d0ca587b861c76
melvin <melvin.melvin.rulit@gmail.com>
2026-08-07 21:17:39 +0300

    валидация истории

---
+++ src/modules/Booking/Http/Requests/BookingHistoryRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Modules\Booking\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class BookingHistoryRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        $statuses = array_merge(config('booking.statuses', []), [
+            'finish_prepayment',
+            'finished_collection',
+        ]);
+
+        return [
+            'status' => ['nullable', 'string', Rule::in($statuses)],
+            'booking_id' => ['nullable', 'integer', 'min:1'],
+            'page' => ['nullable', 'integer', 'min:1'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'status.string' => __('booking.validation.status_must_be_string'),
+            'status.in' => __('booking.validation.status_invalid'),
+            'booking_id.integer' => __('booking.validation.booking_id_must_be_integer'),
+            'booking_id.min' => __('booking.validation.booking_id_min_value'),
+            'page.integer' => __('booking.validation.page_must_be_integer'),
+            'page.min' => __('booking.validation.page_min_value'),
+        ];
+    }
+}
