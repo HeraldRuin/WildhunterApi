2eacccb4691cfe080bc6be336946932317d6b479
melvin <melvin.melvin.rulit@gmail.com>
2026-08-07 21:32:07 +0300

    маршрут подтверждения брони

--- src/modules/Booking/Routes/api/v1/api.php
+++ src/modules/Booking/Routes/api/v1/api.php
@@ -7,6 +7,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/bookings', [BookingController::class, 'store']);
 
     Route::get('/bookings/history', [BookingController::class, 'bookingHistory']);
+    Route::post('/bookings/{code}/confirm', [BookingController::class, 'confirm']);
 
     Route::get('/bookings/{code}/checkout', [BookingController::class, 'checkout']);
     Route::post('/bookings/{code}/checkout', [BookingController::class, 'doCheckout']);
