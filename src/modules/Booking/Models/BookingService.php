58ea4b31236427f4d55df1eac2755b465d7b1f25
melvin <melvin.melvin.rulit@gmail.com>
2026-08-07 21:18:54 +0300

    модель bc_booking_services

---
+++ src/modules/Booking/Models/BookingService.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Modules\Booking\Models;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Modules\Animals\Models\Animal;
+
+
+class BookingService extends Model
+{
+    protected $table = 'bc_booking_services';
+
+    protected $fillable = [
+        'booking_id',
+        'service_type',
+        'calculation_type',
+        'service_id',
+        'type',
+        'count',
+        'price',
+        'comment',
+    ];
+
+    public function booking(): BelongsTo
+    {
+        return $this->belongsTo(Booking::class);
+    }
+    public function hunter(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'hunter_id');
+    }
+    public function animal(): BelongsTo
+    {
+        return $this->belongsTo(Animal::class, 'animal_id');
+    }
+}
