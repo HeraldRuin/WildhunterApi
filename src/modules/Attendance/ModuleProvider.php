3557bfd046c6f46eaf93eae0f7120940e9f1b4c0
melvin <melvin.melvin.rulit@gmail.com>
2026-08-07 21:20:38 +0300

    провайдер

---
+++ src/modules/Attendance/ModuleProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Modules\Attendance;
+
+use Modules\ModuleServiceProvider;
+
+class ModuleProvider extends ModuleServiceProvider
+{
+    public function register(): void
+    {
+    }
+}
