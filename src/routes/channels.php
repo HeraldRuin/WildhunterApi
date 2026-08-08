7ed6eb21fa034b2e151317af5cc83749f69ce56c
melvin <melvin.melvin.rulit@gmail.com>
2026-08-08 14:20:12 +0300

    добавил и подключил канал

---
+++ src/routes/channels.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Broadcast;
+use Modules\Booking\Models\Booking;
+
+Broadcast::channel('bookings.{bookingId}', function (User $user, int $bookingId): bool {
+    return Booking::query()
+        ->whereKey($bookingId)
+        ->where(function ($query) use ($user): void {
+            $query
+                ->where('customer_id', $user->id)
+                ->orWhereHas('bookingHunters', function ($query) use ($user): void {
+                    $query->where('invited_by', $user->id);
+                })
+                ->orWhereHas('bookingHunters.invitations', function ($query) use ($user): void {
+                    $query->where('hunter_id', $user->id);
+                });
+        })
+        ->exists();
+}, ['guards' => ['sanctum']]);
