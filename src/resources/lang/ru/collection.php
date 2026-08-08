d47552b9085b0c08564ec799959009cdd22f6e4c
melvin <melvin.melvin.rulit@gmail.com>
2026-08-08 21:48:39 +0300

    перевод

--- src/resources/lang/ru/collection.php
+++ src/resources/lang/ru/collection.php
@@ -3,8 +3,12 @@
 return [
     'errors' => [
         'booking_collection_not_startable' => 'Сбор можно запустить только для подтверждённой брони',
+        'booking_collection_not_extendable' => 'Продлить можно только активный сбор охотников',
+        'collection_timer_not_found' => 'Таймер сбора охотников не найден',
+        'collection_timer_not_expired' => 'Таймер сбора охотников ещё не истёк',
     ],
     'successes' => [
         'gathering_has_started' => 'Сбор охотников начат',
+        'gathering_has_extended' => 'Сбор охотников продлён',
     ],
 ];
