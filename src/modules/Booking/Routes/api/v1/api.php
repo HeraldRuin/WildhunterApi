154934bf426ef39c9d31bec8bd5e4b0cd6338181
melvin <melvin.melvin.rulit@gmail.com>
2026-08-10 20:09:40 +0300

    маршрут отмены сбора

--- src/modules/Booking/Routes/api/v1/api.php
+++ src/modules/Booking/Routes/api/v1/api.php
@@ -10,6 +10,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/bookings/{code}/confirm', [BookingController::class, 'confirm']);
     Route::post('/bookings/{code}/start-collection', [BookingController::class, 'startCollection']);
     Route::post('/bookings/{code}/extend-collection', [BookingController::class, 'extendCollection']);
+    Route::post('/bookings/{code}/cancel-collection', [BookingController::class, 'cancelCollection']);
     Route::post('/bookings/{code}/accept-invitation', [BookingController::class, 'acceptInvitation']);
     Route::post('/bookings/{code}/decline-invitation', [BookingController::class, 'declineInvitation']);
     Route::post('/bookings/{code}/cancel', [BookingController::class, 'cancel']);
