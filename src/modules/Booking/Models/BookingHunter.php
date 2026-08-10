4c3053cd676bd607aa8cb0147c57a7947dfeaf91
melvin <melvin.melvin.rulit@gmail.com>
2026-08-10 15:18:36 +0300

    методы для замены пользователя

--- src/modules/Booking/Models/BookingHunter.php
+++ src/modules/Booking/Models/BookingHunter.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use App\Models\BaseModel;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class BookingHunter extends BaseModel
 {
@@ -25,6 +26,13 @@ class BookingHunter extends BaseModel
         'is_master' => 'boolean',
     ];
 
+    public function changeCreator(User $user): void
+    {
+        $this->invited_by = $user->id;
+        $this->is_master = $user->hasRole('hunter');
+        $this->creator_role = $user->role->code ?? null;
+    }
+
     public function booking(): BelongsTo
     {
         return $this->belongsTo(Booking::class, 'booking_id');
@@ -33,5 +41,10 @@ class BookingHunter extends BaseModel
     public function invitedBy(): BelongsTo
     {
         return $this->belongsTo(User::class, 'invited_by');
+    }
+
+    public function invitations(): HasMany
+    {
+        return $this->hasMany(BookingHunterInvitation::class, 'booking_hunter_id');
     }
 }
