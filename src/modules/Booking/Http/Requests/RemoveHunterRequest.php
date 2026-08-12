c9c5eab813ea1e3c7851164035834325e84a2ee6
melvin <melvin.melvin.rulit@gmail.com>
2026-08-12 21:13:21 +0300

    валидация удаления охотника

---
+++ src/modules/Booking/Http/Requests/RemoveHunterRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Modules\Booking\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RemoveHunterRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'hunter_id' => ['required', 'integer', 'exists:users,id'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'hunter_id.required' => __('booking.validation.hunter_id_required'),
+            'hunter_id.integer' => __('booking.validation.hunter_id_must_be_integer'),
+            'hunter_id.exists' => __('booking.validation.hunter_id_not_found'),
+        ];
+    }
+}
