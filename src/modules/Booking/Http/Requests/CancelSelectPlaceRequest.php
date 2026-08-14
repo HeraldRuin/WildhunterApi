1a0aab2dba3719f20271868e64f9f7d861093846
melvin <melvin.melvin.rulit@gmail.com>
2026-08-14 17:12:32 +0300

    валидация отмены распределения койко-мест

---
+++ src/modules/Booking/Http/Requests/CancelSelectPlaceRequest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Modules\Booking\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CancelSelectPlaceRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'place_id' => ['required', 'integer', 'min:1'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'place_id.required' => __('booking.validation.place_id_required'),
+            'place_id.integer' => __('booking.validation.place_id_must_be_integer'),
+        ];
+    }
+}
