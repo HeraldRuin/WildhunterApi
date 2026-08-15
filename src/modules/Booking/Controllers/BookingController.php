9500a92a838b2b3a67139f1745c21fbadb5ad0d4
melvin <melvin.melvin.rulit@gmail.com>
2026-08-15 13:56:33 +0300

    необходимое для услуг броней

--- src/modules/Booking/Controllers/BookingController.php
+++ src/modules/Booking/Controllers/BookingController.php
@@ -14,13 +14,28 @@ use Illuminate\Support\Facades\Auth;
 use Modules\Booking\Dto\BookingHistoryData;
 use Modules\Booking\Dto\CreateBookingData;
 use Modules\Booking\Dto\ReplaceHunterData;
+use Modules\Booking\Dto\SelectPlaceData;
+use Modules\Booking\Dto\StoreAdditionalData;
+use Modules\Booking\Dto\StoreFoodData;
+use Modules\Booking\Dto\StorePenaltyData;
+use Modules\Booking\Dto\StorePreparationData;
+use Modules\Booking\Dto\StoreSpendingData;
+use Modules\Booking\Dto\StoreTrophyData;
 use Modules\Booking\Dto\UpdateCustomerNotesData;
 use Modules\Booking\Http\Requests\BookingCreateRequest;
 use Modules\Booking\Http\Requests\BookingHistoryRequest;
+use Modules\Booking\Http\Requests\CancelSelectPlaceRequest;
 use Modules\Booking\Http\Requests\ChangeBookingCustomerRequest;
 use Modules\Booking\Http\Requests\InviteHunterRequest;
 use Modules\Booking\Http\Requests\RemoveHunterRequest;
 use Modules\Booking\Http\Requests\ReplaceHunterRequest;
+use Modules\Booking\Http\Requests\SelectPlaceRequest;
+use Modules\Booking\Http\Requests\StoreAdditionalRequest;
+use Modules\Booking\Http\Requests\StoreFoodRequest;
+use Modules\Booking\Http\Requests\StorePenaltyRequest;
+use Modules\Booking\Http\Requests\StorePreparationRequest;
+use Modules\Booking\Http\Requests\StoreSpendingRequest;
+use Modules\Booking\Http\Requests\StoreTrophyRequest;
 use Modules\Booking\Http\Requests\UpdateCustomerNotesRequest;
 use Modules\Booking\Http\Resources\BookingCheckoutResource;
 use Modules\Booking\Http\Resources\BookingHistoryResource;
@@ -32,6 +47,9 @@ use Modules\Booking\Services\BookingConfirmService;
 use Modules\Booking\Services\BookingCustomerService;
 use Modules\Booking\Services\BookingHistoryService;
 use Modules\Booking\Services\BookingInvitationService;
+use Modules\Booking\Services\BookingMarkPaidService;
+use Modules\Booking\Services\BookingPlaceService;
+use Modules\Booking\Services\BookingServiceManager;
 use Modules\Booking\Services\BookingStoreService;
 use Modules\Booking\Services\PaymentManagerService;
 
@@ -46,7 +64,10 @@ class BookingController extends Controller
         protected BookingCancelService $bookingCancelService,
         protected BookingCollectionService $bookingCollectionService,
         protected BookingInvitationService $bookingInvitationService,
+        protected BookingPlaceService $bookingPlaceService,
+        protected BookingMarkPaidService $bookingMarkPaidService,
         protected PaymentManagerService $paymentManagerService,
+        protected BookingServiceManager $bookingServiceManager,
     ) {
     }
 
@@ -153,6 +174,29 @@ class BookingController extends Controller
                 'id' => $booking->id,
                 'code' => $booking->code,
                 'status' => $booking->status,
+            ],
+        );
+    }
+
+    /**
+     * Отметка брони как оплаченной администратором базы.
+     *
+     * @throws ForbiddenException
+     * @throws NotFoundException
+     * @throws ConflictException
+     */
+    public function markPaid(string $code): JsonResponse
+    {
+        $booking = $this->bookingMarkPaidService->markPaid($code, Auth::user());
+
+        return new SuccessResponse(
+            code: 'booking_marked_paid',
+            domain: 'booking',
+            data: [
+                'id' => $booking->id,
+                'code' => $booking->code,
+                'status' => $booking->status,
+                'is_paid' => (bool) $booking->is_paid,
             ],
         );
     }
@@ -392,6 +436,203 @@ class BookingController extends Controller
                 'code' => $booking->code,
                 'status' => $booking->status,
             ],
+        );
+    }
+
+    /**
+     * Список типов комнат и занятых койко-мест бронирования.
+     *
+     * @throws ForbiddenException
+     * @throws NotFoundException
+     */
+    public function places(string $code): JsonResponse
+    {
+        $data = $this->bookingPlaceService->getPlaces($code, Auth::user());
+
+        return new SuccessResponse(data: $data);
+    }
+
+    /**
+     * Выбор койко-места текущим охотником.
+     *
+     * @throws ConflictException
+     * @throws ForbiddenException
+     * @throws NotFoundException
+     */
+    public function selectPlace(SelectPlaceRequest $request, string $code): JsonResponse
+    {
+        $dto = SelectPlaceData::fromRequest($request);
+        $result = $this->bookingPlaceService->selectPlace($code, $dto, Auth::user());
+
+        return new SuccessResponse(code: $result['code'], domain: 'booking');
+    }
+
+    /**
+     * Отмена своего выбранного койко-места.
+     *
+     * @throws ForbiddenException
+     * @throws NotFoundException
+     */
+    public function cancelSelectPlace(CancelSelectPlaceRequest $request, string $code): JsonResponse
+    {
+        $result = $this->bookingPlaceService->cancelSelectPlace(
+            $code,
+            (int) $request->validated('place_id'),
+            Auth::user(),
+        );
+
+        return new SuccessResponse(code: $result['code'], domain: 'booking');
+    }
+
+    /**
+     * Каталог и уже добавленные услуги брони (набор зависит от роли).
+     *
+     * @throws ConflictException
+     * @throws ForbiddenException
+     * @throws NotFoundException
+     */
+    public function services(string $code): JsonResponse
+    {
+        return new SuccessResponse(
+            data: $this->bookingServiceManager->getServices($code, Auth::user()),
+        );
+    }
+
+    /**
+     * @throws ConflictException
+     * @throws ForbiddenException
+     * @throws NotFoundException
+     */
+    public function storeTrophy(StoreTrophyRequest $request, string $code): JsonResponse
+    {
+        $data = $this->bookingServiceManager->createTrophy(
+            $code,
+            StoreTrophyData::fromRequest($request),
+            Auth::user(),
+        );
+
+        return new SuccessResponse(
+            code: 'service_added',
+            domain: 'booking',
+            data: $data,
+        );
+    }
+
+    /**
+     * @throws ConflictException
+     * @throws ForbiddenException
+     * @throws NotFoundException
+     * @throws ValidationException
+     */
+    public function storePenalty(StorePenaltyRequest $request, string $code): JsonResponse
+    {
+        $data = $this->bookingServiceManager->createPenalty(
+            $code,
+            StorePenaltyData::fromRequest($request),
+            Auth::user(),
+        );
+
+        return new SuccessResponse(
+            code: 'service_added',
+            domain: 'booking',
+            data: $data,
+        );
+    }
+
+    /**
+     * @throws ConflictException
+     * @throws ForbiddenException
+     * @throws NotFoundException
+     */
+    public function storePreparation(StorePreparationRequest $request, string $code): JsonResponse
+    {
+        $data = $this->bookingServiceManager->createOrUpdatePreparation(
+            $code,
+            StorePreparationData::fromRequest($request),
+            Auth::user(),
+        );
+
+        return new SuccessResponse(
+            code: 'service_added',
+            domain: 'booking',
+            data: $data,
+        );
+    }
+
+    /**
+     * @throws ConflictException
+     * @throws ForbiddenException
+     * @throws NotFoundException
+     */
+    public function storeFood(StoreFoodRequest $request, string $code): JsonResponse
+    {
+        $data = $this->bookingServiceManager->createFood(
+            $code,
+            StoreFoodData::fromRequest($request),
+            Auth::user(),
+        );
+
+        return new SuccessResponse(
+            code: 'service_added',
+            domain: 'booking',
+            data: $data,
+        );
+    }
+
+    /**
+     * @throws ConflictException
+     * @throws ForbiddenException
+     * @throws NotFoundException
+     * @throws ValidationException
+     */
+    public function storeAdditional(StoreAdditionalRequest $request, string $code): JsonResponse
+    {
+        $data = $this->bookingServiceManager->createAdditional(
+            $code,
+            StoreAdditionalData::fromRequest($request),
+            Auth::user(),
+        );
+
+        return new SuccessResponse(
+            code: 'service_added',
+            domain: 'booking',
+            data: $data,
+        );
+    }
+
+    /**
+     * @throws ConflictException
+     * @throws ForbiddenException
+     * @throws NotFoundException
+     * @throws ValidationException
+     */
+    public function storeSpending(StoreSpendingRequest $request, string $code): JsonResponse
+    {
+        $data = $this->bookingServiceManager->createSpending(
+            $code,
+            StoreSpendingData::fromRequest($request),
+            Auth::user(),
+        );
+
+        return new SuccessResponse(
+            code: 'service_added',
+            domain: 'booking',
+            data: $data,
+        );
+    }
+
+    /**
+     * @throws ConflictException
+     * @throws ForbiddenException
+     * @throws NotFoundException
+     */
+    public function deleteService(string $code, int $serviceId): JsonResponse
+    {
+        $this->bookingServiceManager->deleteService($code, $serviceId, Auth::user());
+
+        return new SuccessResponse(
+            code: 'service_deleted',
+            domain: 'booking',
         );
     }
 }
