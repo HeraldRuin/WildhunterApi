d494c7f77a989629c28c7ebb941fbc05c9d72278
melvin <melvin.melvin.rulit@gmail.com>
2026-08-15 16:49:31 +0300

    валидация сохранения разделки

---
+++ src/modules/Booking/Http/Requests/StoreFoodRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Modules\Booking\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreFoodRequest extends FormRequest
+{
+    public function rules(): array
+    {
+        return [
+            'count' => ['required', 'integer', 'min:1'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'count.required' => __('booking.validation.service_count_required'),
+            'count.integer' => __('booking.validation.service_count_must_be_integer'),
+            'count.min' => __('booking.validation.service_count_min_value'),
+        ];
+    }
+}
