89437b47d88d0ed4ad113d56bdcd7090f50b722a
melvin <melvin.melvin.rulit@gmail.com>
2026-08-16 12:00:30 +0300

    свойство питание в отеле вывод в ресурсе

--- src/modules/Location/Http/Resources/LocationHotelResource.php
+++ src/modules/Location/Http/Resources/LocationHotelResource.php
@@ -20,6 +20,7 @@ class LocationHotelResource extends BaseJsonResource
             'map_lng'=> $this->resource->map_lng,
             'image_url' => $hotel->getImageUrl('medium') ?: asset('uploads/0000/1/2026/11/14/no_image.png'),
             'star_rate' => $hotel->star_rate,
+            'has_food' => (bool) $hotel->has_food,
             'price' => $hotel->price,
             'review_count' => $hotel->reviews->count(),
             'location' => LocationResource::make($this->resource->location),
