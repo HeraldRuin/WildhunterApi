c49a1473f6265b9dd27afaa1e4f19e039653aece
melvin <melvin.melvin.rulit@gmail.com>
2026-08-16 11:51:58 +0300

    перевод

---
+++ src/resources/lang/ru/calculate.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'errors' => [
+        'no_paid_participants' => 'Нет оплативших участников',
+        'no_hunters' => 'Нет участников охоты',
+        'calculating_not_available' => 'Калькуляция на текущем этапе бронирования недоступна',
+    ],
+];
