b99f58bfdc451c62f2db0cbd1f63799a63f13ba5
melvin <melvin.melvin.rulit@gmail.com>
2026-08-16 17:24:51 +0300

    информация о клиенте вынес в отдельный шаблон

---
+++ src/modules/Booking/Views/emails/parts/panel-customer.blade.php
@@ -0,0 +1,21 @@
+@php
+    $booking->loadMissing('creator');
+    $creator = $booking->creator;
+    $customerFirstName = $booking->first_name ?: $creator?->first_name;
+    $customerLastName = $booking->last_name ?: $creator?->last_name;
+@endphp
+<div class="b-panel">
+    <div class="b-panel-title">{{ __('booking.email.customer_information') }}</div>
+    <div class="b-table-wrap">
+        <table class="b-table" cellspacing="0" cellpadding="0">
+            <tr>
+                <td class="label">{{ __('booking.email.first_name') }}</td>
+                <td class="val">{{ $customerFirstName }}</td>
+            </tr>
+            <tr>
+                <td class="label">{{ __('booking.email.last_name') }}</td>
+                <td class="val">{{ $customerLastName }}</td>
+            </tr>
+        </table>
+    </div>
+</div>
