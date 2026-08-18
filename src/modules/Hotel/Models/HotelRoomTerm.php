858fedd2f16126fbc2f7ab908dcd21e892b89197
melvin <melvin.melvin.rulit@gmail.com>
2026-08-18 17:07:08 +0300

    модель атрибутов номеров

---
+++ src/modules/Hotel/Models/HotelRoomTerm.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Modules\Hotel\Models;
+
+use App\Models\BaseModel;
+
+class HotelRoomTerm extends BaseModel
+{
+    protected $table = 'bc_hotel_room_term';
+
+    protected $fillable = [
+        'term_id',
+        'target_id',
+    ];
+}
