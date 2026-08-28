9471fa790a01c78e44b220f1e18fa026adfce507
melvin <melvin.melvin.rulit@gmail.com>
2026-08-28 15:01:40 +0300

    маршруты базы админа и номера доступности

--- src/modules/Hotel/Routes/api/v1/api.php
+++ src/modules/Hotel/Routes/api/v1/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Modules\Hotel\Controllers\HotelController;
+use Modules\Hotel\Controllers\RoomAvailabilityController;
 
 Route::post('/hotels/offers', [HotelController::class, 'getHotels']);
 Route::post('/hotels/search', [HotelController::class, 'searchHotels']);
@@ -10,5 +11,7 @@ Route::post('/hotels/rooms/check-availability', [HotelController::class, 'checkA
 Route::get('/hotels/{location}/{slug}', [HotelController::class, 'getHotel']);
 
 Route::middleware('auth:sanctum')->group(function () {
-
+    Route::get('/hotels/manage', [HotelController::class, 'manageList']);
+    Route::get('/rooms', [RoomAvailabilityController::class, 'index']);
+    Route::get('/rooms/availability', [RoomAvailabilityController::class, 'loadDates']);
 });
