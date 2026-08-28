64698ed4065a1210224e71fea74e572e6c1db72d
melvin <melvin.melvin.rulit@gmail.com>
2026-08-28 17:30:09 +0300

    маршруты доп услуг

---
+++ src/modules/Attendance/Routes/api/v1/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Modules\Attendance\Controllers\AdditionalController;
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/services/additionals', [AdditionalController::class, 'index']);
+    Route::post('/services/additionals', [AdditionalController::class, 'store']);
+    Route::put('/services/additionals/{additional}', [AdditionalController::class, 'update']);
+    Route::delete('/services/additionals/{additional}', [AdditionalController::class, 'destroy']);
+});
