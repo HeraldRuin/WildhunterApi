fd9175ec70612749386ab9a7d68e43f52808ca36
melvin <melvin.melvin.rulit@gmail.com>
2026-08-29 10:12:39 +0300

    трейт

--- src/app/Traits/HasHotelAnimalPrice.php
+++ src/app/Traits/HasHotelAnimalPrice.php
@@ -11,4 +11,27 @@ trait HasHotelAnimalPrice
     {
         return $this->morphMany(HotelAnimalPrice::class, 'priceable');
     }
+
+    public function setHotelPrice(int $hotelId, $price)
+    {
+        return HotelAnimalPrice::updateOrCreate(
+            [
+                'hotel_id' => $hotelId,
+                'priceable_id' => $this->id,
+                'priceable_type' => get_class($this),
+            ],
+            [
+                'price' => $price ?? null,
+            ]
+        );
+    }
+
+    public function priceForHotel(int $hotelId)
+    {
+        $price = $this->hotelPrices
+            ->where('hotel_id', $hotelId)
+            ->first();
+
+        return $price ? $price->price : null;
+    }
 }
