4ce317a71ed1e8650f3eaad98c2994c5190dfc9f
melvin <melvin.melvin.rulit@gmail.com>
2026-08-30 09:46:46 +0300

    маршрут сохранение фотографий

--- src/modules/Media/Routes/api/v1/api.php
+++ src/modules/Media/Routes/api/v1/api.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Modules\Media\Controllers\MediaController;
 
 Route::middleware('auth:sanctum')->group(function () {
-
- });
+    Route::post('/media/store', [MediaController::class, 'store']);
+});
