4dd4b02564f72ce52b6869c3eeadbc8c997bc3d1
melvin <melvin.melvin.rulit@gmail.com>
2026-08-30 09:43:48 +0300

    перевод

---
+++ src/resources/lang/ru/media.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'validation' => [
+        'file_required' => 'Выберите файл для загрузки',
+        'file_must_be_file' => 'Поле должно быть файлом',
+        'file_must_be_image' => 'Файл должен быть изображением',
+        'file_mimes' => 'Допустимые форматы: jpeg, jpg, png, webp, gif',
+        'file_max' => 'Размер файла не должен превышать 10 МБ',
+    ],
+    'successes' => [
+        'media_uploaded' => 'Файл загружен',
+    ],
+];
