8ed4e777352f7d8c2b65c19b5cfe78f46aa5fcdd
melvin <melvin.melvin.rulit@gmail.com>
2026-09-01 20:15:38 +0300

    список системные услуги

--- src/modules/Attendance/Routes/api/v1/api.php
+++ src/modules/Attendance/Routes/api/v1/api.php
@@ -5,6 +5,7 @@ use Modules\Attendance\Controllers\AdditionalController;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/services/additionals', [AdditionalController::class, 'index']);
+    Route::get('/services/system', [AdditionalController::class, 'systemIndex']);
     Route::post('/services/additionals', [AdditionalController::class, 'store']);
     Route::put('/services/additionals/{additional}', [AdditionalController::class, 'update']);
     Route::delete('/services/additionals/{additional}', [AdditionalController::class, 'destroy']);
