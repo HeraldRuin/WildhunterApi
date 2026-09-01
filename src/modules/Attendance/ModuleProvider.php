abfad0d3f5dc77b0e76bcb24a999e642131a5570
melvin <melvin.melvin.rulit@gmail.com>
2026-09-01 19:13:01 +0300

    регистрация модуля мигация в доп услуги

--- src/modules/Attendance/ModuleProvider.php
+++ src/modules/Attendance/ModuleProvider.php
@@ -6,6 +6,11 @@ use Modules\ModuleServiceProvider;
 
 class ModuleProvider extends ModuleServiceProvider
 {
+    public function boot(): void
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/Migrations');
+    }
+
     public function register(): void
     {
         $this->app->register(RouterServiceProvider::class);
